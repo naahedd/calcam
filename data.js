b7b4aace30930d6144f699552734312f6024ac2a
unknown <mohammedragab470@gmail.com>
2025-01-08 22:10:42 -0600

    init commit

---
+++ data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    May: [
+      {
+        img: "./assets/img1.jpg",
+        h1: "A Moment of Happiness",
+        copy: "A moment of happiness, you and I sitting on the verandah, apparently two, but one in soul, you and I. We feel the flowing water of life here, you and I, with the garden's beauty and the birds singing. The stars will be watching us, and we will show them what it is to be a thin crescent moon. You and I unselfed, will be together, indifferent to idle speculation, you and I. The parrots of heaven will be cracking sugar as we laugh together, you and I. In one form upon this earth, and in another form in a timeless sweet land.",
+      },
+      {
+        img: "./assets/img2.jpg",
+        h1: "Because I Cannot Sleep",
+        copy: "Because I cannot sleep I make music at night. I am troubled by the one whose face has the color of spring flowers. I have neither sleep nor patience, neither a good reputation nor disgrace. A thousand robes of wisdom are gone. All my good manners have moved a thousand miles away. The heart and the mind are left angry with each other. The stars and the moon are envious of each other. Because of this alienation the physical universe is getting tighter and tighter. The moon says, 'How long will I remain suspended without a sun?' Without Love's jewel inside of me, let the bazaar of my existence be destroyed stone by stone. O Love, You who have been called by a thousand names, You who know how to pour the wine into the chalice of the body, You who give culture to a thousand cultures, You who are faceless but have a thousand faces, O Love, You who shape the faces of Turks, Europeans, and Zanzibaris, give me a glass from Your bottle, or a handful of bheng from Your Branch. Remove the cork once more. The we'll see a thousand chiefs prostrate themselves, and a circle of ecstatic troubadours will play. Then the addict will be breed of craving. and will be resurrected, and stand in awe till Judgement Day.",
+      },
+      {
+        img: "./assets/img3.jpg",
+        h1: "I Swear",
+        copy: "I swear, since seeing Your face, the whole world is fraud and fantasy The garden is bewildered as to what is leaf or blossom. The distracted birds can't distinguish the birdseed from the snare. A house of love with no limits, a presence more beautiful than venus or the moon, a beauty whose image fills the mirror of the heart.",
+      },
+      {
+        img: "./assets/img4.jpg",
+        h1: "Any Lifetime",
+        copy: "Any lifetime that is spent without seeing the master Is either death in disguise or a deep sleep. The water that pollutes you is poison; The poison that purifies you is water.",
+      },
+    ],
+   }
+  ];
+  
+export default data;
---
+++ gradient.js
@@ -0,0 +1,7 @@
+import { Gradient } from './gradient.min.js';
+
+// Create your instance
+const gradient = new Gradient();
+
+// Call `initGradient` with the selector to your canvas
+gradient.initGradient('#gradient-canvas'); 
